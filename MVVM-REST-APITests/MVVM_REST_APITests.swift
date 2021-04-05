7b07ad27419328d4765c880800e5cfbdefa4fcd0
Angelo <devsangelo@gmail.com>
2021-04-05 09:02:02 +0800

    Initial Commit

---
+++ MVVM-REST-APITests/MVVM_REST_APITests.swift
@@ -0,0 +1,33 @@
+//
+//  MVVM_REST_APITests.swift
+//  MVVM-REST-APITests
+//
+//  Created by Ruthlyn Huet on 4/5/21.
+//
+
+import XCTest
+@testable import MVVM_REST_API
+
+class MVVM_REST_APITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
